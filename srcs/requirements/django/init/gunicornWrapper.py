4a798d1f5321711a29d0192833cb38d0a5c2f8c3
Mal-back <purmerinos@protonmail.com>
2024-08-23 14:43:06 +0200

    Implement hook for SIGTERM in django app if dump data is set -- Move init script to django/init for more readability

--- srcs/requirements/django/init/gunicornWrapper.py
+++ srcs/requirements/django/init/gunicornWrapper.py
@@ -5,8 +5,9 @@ import time
 stopme = False
 
 def sig_handler(sig, frame):
+    global stopme
     stopme = True
-#
+
 signal.signal(signal.SIGTERM, sig_handler)
 proc = subprocess.Popen(['gunicorn', '--bind', '0.0.0.0:8443', '--reload', '--certfile=/certs/django.crt',
                          '--keyfile=/certs/django.key', '--cert-reqs=2', '--ca-certs=/certs/nginx_client.crt',
@@ -15,6 +16,5 @@ while (stopme == False) :
     pass
 proc.terminate()
 proc.wait()
-print('Dumping Fixture')
-proc = subprocess.Popen(['python3', 'manage.py', 'data/fixture.json'])
+proc = subprocess.Popen(['python3', 'manage.py', 'dumpdata', '-o', 'data/fixture.json'])
 proc.wait()
