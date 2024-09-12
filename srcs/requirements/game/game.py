f1a27efa0955f4d08808528487bdbdceb9a8ff0f
Leo Doyen--Clement <ldoyen--@paul-f5Ar4s2.clusters.42paris.fr>
2024-09-12 19:02:39 +0200

    game

--- srcs/requirements/game/game.py
+++ srcs/requirements/game/game.py
@@ -2,43 +2,64 @@
 
 from enum import Enum
 
-class Player():
-
-	def __init__(self, number):
-		self.number = number
-
-# class Game():
-
-
-# class Ball():
-
-class CoordLimits(Enum):
+class Const(Enum):
 	MAX_X = 500
 	MIN_X = 0
 	MAX_Y = 250
 	MIN_Y = -250
+	MAX_PLAYER = 2
+
+class Player():
+	def __init__(self, number : int, score : int = 0):
+		self.pad = Pad(0, 0)
+		self.score = score
+		self.number = number
+	
+	@property
+	def number(self):
+		return self._number
+	
+	@number.setter
+	def number(self, value):
+		if not value in range (1, Const.MAX_PLAYER.value):
+			raise ValueError("exced max player value")
+		self._number = value
+
+	@property
+	def score(self):
+		return self._score
+
+	@score.setter
+	def score(self, value):
+		self._score = value
 
 class Pad():
-
 	def __init__(self, pos_x : int, pos_y : int):
-		if not pos_x in range(CoordLimits.MIN_X.value, CoordLimits.MAX_X.value):
-			raise ValueError("x out of range")
-		self._x = pos_x
-		self._y = pos_y
+		self.x = pos_x
+		self.y = pos_y
 	
-	def get_x(self):
+	@property
+	def x(self):
 		return self._x
 	
-	def set_x(self, new_x):
-		self._x = new_x
+	@x.setter
+	def x(self, value):
+		if not value in range(Const.MIN_X.value, Const.MAX_X.value):
+			raise ValueError("x Pad out of range")
+		self._x = value
 	
-	def get_y(self):
+	@property
+	def y(self):
 		return self._y
 	
-	def set_y(self, new_y):
-		self._y = new_Y
-		
+	@y.setter
+	def y(self, value):
+		if not value in range(Const.MIN_Y.value, Const.MAX_Y.value):
+			raise ValueError("y Pad out of range")
+		self._y = value
 
-myPad = Pad(15, 2)
-print(myPad._x)
 
+leo = Player(1)
+print(leo.score)
+leo.score += 1
+print(leo.score)
