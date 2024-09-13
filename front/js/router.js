053a5846ad97c5a2d8fb6ee6daef886bb6449478
LE Thomas <thomas.le.tle@gmail.com>
2024-09-13 06:13:49 +0200

    css xav 4 u

---
+++ front/js/router.js
@@ -0,0 +1,27 @@
+const route = (event) => {
+	event = event || window.event;
+	event.preventDefault();
+	window.history.pushState({}, "", event.target.href);
+	handleLocation();
+};
+
+const routes = {
+	404: "404.html",
+	"/": "pages/home.html",
+	"/game": "/pages/game.html",
+	"/profile": "/pages/profile.html",
+	"/auth": "/pages/auth.html",
+};
+
+const handleLocation = async () => {
+	const path = window.location.pathname;
+	const route = routes[path] || routes[404];
+	const html = await fetch(route).then((data) => data.text());
+	console.log(html)
+	document.getElementById("test").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
--- srcs/volumes/auth/auth_app/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-class IsOwner(permissions.BasePermission):
-    """Check whethr pk in JWT token is equal to pk of the requested obj"""
-    def has_object_permission(self, request, view, obj):
-        return obj.id == request.user.id
-        
