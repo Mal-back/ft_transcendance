662751a16352ebf7d42ffa0be7709c41f7303d3f
Leo Doyen--Clement <ldoyen--@made-f0Ar2s6.clusters.42paris.fr>
2024-09-14 17:01:36 +0200

    save game

--- srcs/requirements/game/game.py
+++ srcs/requirements/game/game.py
@@ -1,43 +1,49 @@
 # coding: utf-8
 
 from enum import Enum
+import turtle
+import time
 
 class Const(Enum):
-	MAX_X = 500
-	MIN_X = 0
-	MAX_Y = 250
-	MIN_Y = -250
+	MAX_X = 300
+	MIN_X = -300
+	MAX_Y = 200
+	MIN_Y = -200
 	MAX_PLAYER = 2
+	MAX_SCORE = 10
+	PAD_wIDTH = 30
+	PAD_HEIGHT = 80
+	PAD_DISTANCE = 60
 
 class Pad():
 	def __init__(self, pos_x : int, pos_y : int):
-		self.x = pos_x
-		self.y = pos_y
+		self.x = int(pos_x)
+		self.y = int(pos_y)
 	
 	@property
 	def x(self):
 		return self._x
 	
 	@x.setter
-	def x(self, value):
+	def x(self, value : int):
 		if not value in range(Const.MIN_X.value, Const.MAX_X.value):
 			raise ValueError("x Pad out of range")
-		self._x = value
+		self._x = int(value)
 	
 	@property
 	def y(self):
 		return self._y
 	
 	@y.setter
-	def y(self, value):
+	def y(self, value : int):
 		if not value in range(Const.MIN_Y.value, Const.MAX_Y.value):
 			raise ValueError("y Pad out of range")
-		self._y = value
+		self._y = int(value)
 
 class Ball():
 	def __init__(self, pos_x : int, pos_y : int):
-		self.x = pos_x
-		self.y = pos_y
+		self.x = int(pos_x)
+		self.y = int(pos_y)
 	
 	@property
 	def x(self):
@@ -60,9 +66,10 @@ class Ball():
 		self._y = value
 
 class Player():
-	def __init__(self, number : int, name : str, score : int = 0):
-		self.pad = Pad(0, 0)
-		self.score = score
+	def __init__(self, number : int, name : str, score : int = 0, pad : Pad = Pad(0, 0)):
+		self.pad = pad
+		self.score = int(score)
+		print(type(score), type(self.score))
 		self.number = number
 		self.name = str(name)
 	
@@ -92,11 +99,19 @@ class Player():
 	def score(self, value : int):
 		self._score = value
   
+	@property
+	def pad(self):
+		return self._pad
+
+	@pad.setter
+	def pad(self, newPad : Pad):
+		self._pad = newPad
+  
 class Game():
     def __init__(self, player1 : Player, player2 : Player):
         self.player1 = player1
         self.player2 = player2
-        self.ball = Ball(50, 50)
+        self.ball = Ball(0, "salut")
         
     @property
     def player1(self):
@@ -123,11 +138,31 @@ class Game():
         self._ball = newBall
     
     def printInfo(self):
-        print("Player 1 :\n\tName : " + self.player1.name)
+        print("Player 1 :\n\tName : " + self.player1.name
+              + "\n\tScore : " + str(self.player1.score)
+              + "\n\tX Pad : " + str(self.player1.pad.x)
+              + "\n\tY Pad : " + str(self.player1.pad.y))
+        print("Player 2 :\n\tName : " + self.player2.name
+              + "\n\tScore : " + str(self.player2.score)
+              + "\n\tX Pad : " + str(self.player2.pad.x)
+              + "\n\tY Pad : " + str(self.player2.pad.y))
+        print("Ball : \n\tX Ball : " + str(self.ball.x)
+              + "\n\tY Ball : " + str(self.ball.y))
+        
+    def resetBallPosition(self):
+        self.ball.x = 0
+        self.ball.y = 0
+        
+    def start(self):
+        while True:
+            continue
 
-
-bob = Player(1, "Bob")
-patrick = Player(2, "Patrick")
+bob = Player(1, "Bob", 0, Pad(Const.MIN_X.value + Const.PAD_DISTANCE.value, 0))
+patrick = Player(2, "Patrick", 0, Pad(Const.MAX_X.value - Const.PAD_DISTANCE.value, 0))
 game = Game(bob, patrick)
 game.printInfo()
+
+ball = turtle.Turtle()
+ball.dx = -1
+print(ball.dx)
 
