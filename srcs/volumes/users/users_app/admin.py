8a102e0d843b38090753fbf05af52a229e5ab97f
Leo Doyen--Clement <ldoyen--@made-f0Ar4s2.clusters.42paris.fr>
2024-09-17 18:23:05 +0200

    save admin

--- srcs/volumes/users/users_app/admin.py
+++ srcs/volumes/users/users_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+# from users_app.models import PublicUser
+# admin.site.register(PublicUser)
+# class PublicUserInLine(admin.TabularInline):
+#     model = PublicUser
+
+# @admin.register(PublicUser)
+# class PublicUserAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'profilPic', 'account_creation', 'last_seen_online',
+#                     'friends', 'single_games_won', 'single_games_lost',
+#                     'single_games_lost', 'tournament_games_won', 'tournament_games_lost',
+#                     'tournaments_won', 'tournaments_lost')
+#     fields = ['username', 'profilPic', 'account_creation', 'last_seen_online',
+#                     'friends', 'single_games_won', 'single_games_lost',
+#                     'single_games_lost', 'tournament_games_won', 'tournament_games_lost',
+#                     'tournaments_won', 'tournaments_lost']
+#     inlines = [PublicUserInLine]
+    
+# admin.site.register(PublicUser, PublicUserAdmin)
 # Register your models here.
