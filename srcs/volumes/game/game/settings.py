321b0291d1da44292d193568ef1959af07c46a3c
Leo Doyen--Clement <ldoyen--@made-f0Ar3s2.clusters.42paris.fr>
2024-09-18 19:39:49 +0200

    settings django app game

--- srcs/volumes/game/game/settings.py
+++ srcs/volumes/game/game/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9uo)@8oen0xcrjh&(acb(ge&hg@dt_5^7gx7i9m(r2aqf5^%lu'
+SECRET_KEY = os.getenv('DJANGO_GAME_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['game', 'users', 'localhost', 'auth']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
@@ -37,9 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    # 'game_app.apps.GameAppConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +55,20 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'game_app.middleware.SetRequestHostMiddleware',
+    # 'users_app.middleware.UpdateLastUserActivityMiddleware'
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'x-csrftoken',
+    'authorization',
+    'accept',
+    'origin',
+    'user-agent',
+    'x-requested-with',
 ]
 
 ROOT_URLCONF = 'game.urls'
@@ -75,8 +97,18 @@ WSGI_APPLICATION = 'game.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('GAME_DB_NAME'),
+        'USER': os.getenv('GAME_DB_USER'),
+        'PASSWORD': os.getenv('GAME_DB_PASSWORD'),
+        'HOST': os.getenv('GAME_DB_HOST'),
+        'PORT': os.getenv('GAME_DB_PORT'),
+        'OPTIONS': {
+			'sslmode' : 'require',
+			'sslcert' : '/certs/game_client.crt',
+			'sslkey' : '/certs/game_client.key',
+			'sslrootcert' : '/certs/ca.crt',
+		}
     }
 }
 
@@ -99,6 +131,48 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+            ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+        'PAGE_SIZE': 10,
+        }
+
+def get_jwt_keys(key): 
+    with open(key, 'r') as keyfile :
+        return keyfile.read()
+
+SIMPLE_JWT = {
+            "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+            "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+            "ROTATE_REFRESH_TOKENS": True,
+            "BLACKLIST_AFTER_ROTATION": True,
+            "UPDATE_LAST_LOGIN": False,
+
+            "ALGORITHM": "RS512",
+            # "VERIFYING_KEY": get_jwt_keys('/certs/jwt_public.pem'),
+            "AUDIENCE": None,
+            "ISSUER": None,
+            "JSON_ENCODER": None,
+            "JWK_URL": None,
+            "LEEWAY": 0,
+
+            "AUTH_HEADER_TYPES": ("Bearer",),
+            "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+            "USER_ID_FIELD": "id",
+            "USER_ID_CLAIM": "user_id",
+            "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+            "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+            "TOKEN_TYPE_CLAIM": "token_type",
+            "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+            "JTI_CLAIM": "jti",
+
+            "TOKEN_OBTAIN_SERIALIZER": "game_app.serializers.MyTokenObtainPairSerializer",
+        }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
@@ -115,6 +189,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticRoot'
 STATIC_URL = 'static/'
 
 # Default primary key field type
