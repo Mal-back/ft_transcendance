a00e653d83ce99ce5c05c0f44a385d9de57557d7
Mal-back <purmerinos@protonmail.com>
2024-10-06 14:29:11 +0200

    Add link for adding user as friend

--- srcs/volumes/users/users_app/serializers.py
+++ srcs/volumes/users/users_app/serializers.py
@@ -15,12 +15,13 @@ class PublicUserDetailSerializer(serializers.ModelSerializer):
     overall_win_rate = serializers.SerializerMethodField()
     is_online = serializers.SerializerMethodField()
     is_your_friend = serializers.SerializerMethodField()
+    friend_management = serializers.SerializerMethodField()
     class Meta:
         model = PublicUser
         fields =  ['username','profilePic', 'account_creation', 'is_online',
                    'single_games_won', 'single_games_lost', 'tournament_games_won',
                    'tournament_games_lost', 'tournaments_won', 'tournaments_lost',
-                   'total_games', 'total_tournaments', 'is_your_friend',
+                   'total_games', 'total_tournaments', 'is_your_friend', 'friend_management',
                   'total_single_games', 'overall_wins', 'overall_losts',
                   'tournament_win_rate', 'single_games_win_rate',
                   'tournament_games_win_rate', 'overall_win_rate',
@@ -68,7 +69,7 @@ class PublicUserDetailSerializer(serializers.ModelSerializer):
 
     def get_is_your_friend(self, obj):
         request = self.context.get('request')
-        if request.user is not None and request.user.is_authenticated:
+        if request.user is not None:
             if obj.id == request.user.id:
                 return None
             try:
@@ -81,6 +82,25 @@ class PublicUserDetailSerializer(serializers.ModelSerializer):
         else :
             return None
 
+    def get_friend_management(self, obj):
+        request = self.context.get('request')
+        if request.user is not None and request.user.id != obj.id:
+            try:
+                logged_user = PublicUser.objects.get(pk=request.user.id)
+            except PublicUser.DoesNotExist:
+                return None
+
+            if logged_user.friends.filter(id=obj.pk).exists():
+                return {
+                    'action': 'remove',
+                    'url': f'/api/users/{logged_user.username}/friends/delete/{obj.username}/'
+                }
+            else:
+                return {
+                    'action': 'add',
+                    'url': f'/api/users/{logged_user.username}/friends/add/{obj.username}/'
+                }
+        return None
 
 
 class PublicUserListSerializer(serializers.ModelSerializer):
@@ -90,9 +110,10 @@ class PublicUserListSerializer(serializers.ModelSerializer):
             view_name='user-detail',
             lookup_field='username'
             )
+    friend_management = serializers.SerializerMethodField()
     class Meta:
         model = PublicUser
-        fields = ['username', 'profilePic', 'is_online', 'full_profile', 'is_your_friend']
+        fields = ['username', 'profilePic', 'is_online', 'full_profile', 'is_your_friend', 'friend_management']
 
     def get_is_online(self, obj): 
         if obj.last_seen_online == None or now() - obj.last_seen_online > timedelta(minutes=5):
@@ -101,8 +122,35 @@ class PublicUserListSerializer(serializers.ModelSerializer):
     
     def get_is_your_friend(self, obj):
         request = self.context.get('request')
-        if request.user and obj.id == request.user.id:
+        if request.user is not None:
+            if obj.id == request.user.id:
+                return None
+            try:
+                user = PublicUser.objects.get(pk=request.user.id)
+            except PublicUser.DoesNotExist:
+                return None
+            if user.friends.filter(id=obj.pk).exists():
+                return True
+            return False
+        else :
             return None
-        if request.user.friends.filter(id=obj.pk).exists():
-            return True
-        return False
+
+    def get_friend_management(self, obj):
+        request = self.context.get('request')
+        if request.user is not None and request.user.id != obj.id:
+            try:
+                logged_user = PublicUser.objects.get(pk=request.user.id)
+            except PublicUser.DoesNotExist:
+                return None
+
+            if logged_user.friends.filter(id=obj.pk).exists():
+                return {
+                    'action': 'remove',
+                    'url': f'/api/users/{logged_user.username}/friends/delete/{obj.username}/'
+                }
+            else:
+                return {
+                    'action': 'add',
+                    'url': f'/api/users/{logged_user.username}/friends/add/{obj.username}/'
+                }
+        return None
