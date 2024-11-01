15246132212ba69c0e29ca9bb854c90a2c6001a1
LE Thomas <thomas.le.tle@gmail.com>
2024-11-01 14:56:42 +0100

    css to get

--- game/check.sh
+++ game/check.sh
@@ -1,8 +1,8 @@
-for file in ../front/css/*; do
+for file in css/*; do
     filename=$(basename "$file")
-    if [ -f "css/$filename" ]; then
+    if [ -f "css copy/$filename" ]; then
         echo "Comparing $filename:"
-        diff "$file" "css/$filename"
+        diff "$file" "css copy/$filename"
         echo
     else
         echo "$filename does not exist in dir2"
