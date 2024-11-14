2e078b1bb4632acc3c4841677facf3bb6eb2edcc
Mal-back <purmerinos@protonmail.com>
2024-11-14 14:55:40 +0100

    Add slashes

--- srcs/volumes/auth/auth_app/urls.py
+++ srcs/volumes/auth/auth_app/urls.py
@@ -7,16 +7,16 @@ from . import views
 
 urlpatterns = [
         path('', views.UserCreateView.as_view(), name='auth-create'),
-        path('login', TokenObtainPairView.as_view(), name='auth-login'),
-        path('refresh', TokenRefreshView.as_view() , name='auth-refresh'),
-        path('logout', TokenBlacklistView.as_view(), name='auth-logout'),
-        path('delete/<str:username>', views.UserDeleteView.as_view(), name='auth-delete'),
-        path('update/<str:username>', views.UserUpdateView.as_view(), name='auth-update'),
-        path('password/<str:username>', views.PasswordUpdateView.as_view(), name='password-update'),
+        path('login/', TokenObtainPairView.as_view(), name='auth-login'),
+        path('refresh/', TokenRefreshView.as_view() , name='auth-refresh'),
+        path('logout/', TokenBlacklistView.as_view(), name='auth-logout'),
+        path('delete/<str:username>/', views.UserDeleteView.as_view(), name='auth-delete'),
+        path('update/<str:username>/', views.UserUpdateView.as_view(), name='auth-update'),
+        path('password/<str:username>/', views.PasswordUpdateView.as_view(), name='password-update'),
         path('internal/auth/', views.ServiceJWTObtainPair.as_view(), name='service-auth'),
         path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
         path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
         path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
         path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-        path("<str:username>", views.UserDetailView.as_view(), name='auth-detail'),
+        path("<str:username>/", views.UserDetailView.as_view(), name='auth-detail'),
 ]
